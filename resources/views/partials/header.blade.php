9f50bc0f24313be4329161818fdc82b48cfccca9
RUDIWER <rudi.werner@pandora.be>
2016-12-12 19:03:21 +0100

    Added Favicon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+
     <title>{{ config('app.name', 'Lara') }}</title>
     <!-- JQUERY -->
     <script   src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
