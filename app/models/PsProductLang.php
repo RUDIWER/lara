96f38f9bfcf0f1621873ed55c8e60658c4728d5c
RUDIWER <rudi.werner@pandora.be>
2017-01-08 18:01:08 +0100

    error in product_lang

--- app/models/PsProductLang.php
+++ app/models/PsProductLang.php
@@ -8,4 +8,19 @@ class PsProductLang extends Model
     protected $table = 'ps_product_lang';
     protected $primaryKey = 'id_product';
     public $timestamps = false;
+    protected $fillable=[
+        'id_product',
+        'id_shop',
+        'id_lang',
+        'description',
+        'description_short',
+        'link_rewrite',
+        'meta_description',
+        'meta_keywords',
+        'meta_title',
+        'name',
+        'available_now',
+        'available_later'
+    ];
 }
+
